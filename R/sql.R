14147c89fbc5f9d16d9d16579c1e6508037734d3
Joshua Barratt <joshua@barratt.pro>
2018-03-10 13:01:05 +0100

    update make.sql.names to follow all the rules for postgres and sql identifiers and that they will never clash with key words and to follow the lowercase identifiers convention.

--- R/sql.R
+++ R/sql.R
@@ -1,9 +1,22 @@
-make.sql.names <- function(df,max.len=64){
+make.sql.names <- function(df,max.len=63){
   ns <- sapply(df,function(x){
     n <- name(x)
-    if(type(x)=="select all that apply") n <- paste(n,selected(x),sep="/")
+    if(type(x)=="select all that apply") n <- paste(n,selected(x),sep="_")
     n
   })
-  ns <- substr(ns,1,max.len-8)
-  make.unique(ns)
+  ns <- tolower(ns)
+  ns <- ifelse(substr(ns,nchar(ns),nchar(ns))=="_" |
+           substr(ns,1,1)=="_" |
+           grepl("[0-9]",ns), ns, paste0(ns,"_"))
+  ns <- ifelse(grepl("^[0-9]",ns),paste0("_",ns),ns)
+  ns <- gsub("[^A-z0-9_]+","_",ns)
+  i <- 0
+  repeat{
+    ns <- substr(ns,1,max.len-i)
+    ns <- make.unique(ns)
+    if(max(sapply(ns,nchar)<=max.len)) break
+    i <- i+1
+  }
+  cat(i,"\n")
+  ns
 }
